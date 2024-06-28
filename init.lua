9c4fd16dfeff20e95859bf1b0a7bba08f10414b2
Krissada Sarawit <69339852+tsphere101@users.noreply.github.com>
2024-06-28 21:41:34 +0700

    add formatter for c and cpp

--- init.lua
+++ init.lua
@@ -568,6 +568,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clangd' },
+        cpp = { 'clangd' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
         --
